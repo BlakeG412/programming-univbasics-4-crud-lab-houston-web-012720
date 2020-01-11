71a2cfb3d3bef49fec51bd2b043887c064a66502
Blake Gaal <bag.blakeandrewgaal@gmail.com>
2020-01-11 02:41:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["nike", "adidas", "asic"]
 end
 
 def remove_element_from_start_of_array(array)
