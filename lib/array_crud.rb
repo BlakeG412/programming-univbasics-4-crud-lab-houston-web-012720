3c37880bc256ef67f2943b9b24a7382c0fcf327e
Blake Gaal <bag.blakeandrewgaal@gmail.com>
2020-01-11 02:54:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["nike", "adidas", "asic"]
+  nike = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
