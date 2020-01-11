2bd7bfa5485af87fa64d214f92db4f28516eb78d
Blake Gaal <bag.blakeandrewgaal@gmail.com>
2020-01-11 02:37:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["nike", "adidas", "asic"]
-  array.push
+  array.push("mizuno")
 end
 
 def add_element_to_start_of_array(array, element)
